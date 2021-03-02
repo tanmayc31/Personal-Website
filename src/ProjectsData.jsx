4b22a467b0b432649b8371f977d477b9273ebbb2
Tanmay <tanmaychandan31@gmail.com>
2021-03-02 14:07:30 +0530

    made some changes

--- src/ProjectsData.jsx
+++ src/ProjectsData.jsx
@@ -2,27 +2,27 @@ import ToDoListImg from "./images/ToDo-List.png";
 import GoogleKeepClone from "./images/Google-keep.png";
 const Img1 = "https://picsum.photos/300";
 
-const ToDoListProject = () => {
-    window.location.href="https://tanmayc31.github.io/ToDoList-app/";
-};
-const googleKeepCloneProject = () => {
-    window.location.href="https://tanmayc31.github.io/google-keep-app-clone/";
-};
+//const ToDoListProject = () => {
+//    window.location.href="https://tanmayc31.github.io/ToDoList-app/";
+//};
+//const googleKeepCloneProject = () => {
+//    window.location.href="https://tanmayc31.github.io/google-keep-app-clone/";
+//};
 
 const ProjectsData = [
     {
         imgsrc: ToDoListImg, 
         title: "To-Do List",
       
-        projectlink: ToDoListProject,
+        projectlink: () => window.open('https://tanmayc31.github.io/ToDoList-app/',"_blank"),
     
     },
     {
         imgsrc: GoogleKeepClone,
         title: "Google Keep Clone",
        
-        projectlink: googleKeepCloneProject,
-      
+        projectlink: () => window.open('https://tanmayc31.github.io/google-keep-app-clone/',"_blank"),
+
     },
     {
         imgsrc: Img1,
